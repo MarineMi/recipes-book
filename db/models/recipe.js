dce1f5d20fefb3bb6fb2ff13b146a44df35a0d7d
MarineMi <m.g.vasyukova@gmail.com>
2023-06-07 10:39:58 +0300

    db

---
+++ db/models/recipe.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Recipe extends Model {
+    static associate({ User }) {
+      this.belongsTo(User, { foreignKey: 'user_id' });
+    }
+  }
+  Recipe.init(
+    {
+      user_id: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+      },
+      recipe: {
+        allowNull: false,
+        type: DataTypes.TEXT,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Recipe',
+    }
+  );
+  return Recipe;
+};
