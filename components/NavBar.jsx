d0f4a256d3175189339c8510c1ece6db2b72c2a5
MarineMi <m.g.vasyukova@gmail.com>
2023-06-09 13:38:32 +0300

    @media

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -4,7 +4,7 @@ function NavBar({ user }) {
   return (
     <nav className='navbar navbar-expand-lg navbar-light bg-light navbar1'>
       <div className='container-fluid'>
-        <div className='collapse navbar-collapse' id='navbarSupportedContent'>
+        <div className='navbar-collapse' id='navbarSupportedContent'>
           <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
             <li className='nav-item'>
               <a className='nav-link active main' aria-current='page' href='/'>
